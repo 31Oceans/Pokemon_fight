1849d2ae6d680124ea3a9092fbbf94bb13eb8492
31Oceans <sahil.ghosalkar@gmail.com>
2023-04-28 14:51:31 -0700

    first commit

---
+++ pokemon_fight.py
@@ -0,0 +1,17 @@
+import random
+
+#make health
+
+#make damage functions
+
+#make move/attack/heal options
+
+#add retreat option
+
+#let person choose what option
+
+#item options
+
+#make an opponent
+
+#have the game go till all 6 pokemon die
